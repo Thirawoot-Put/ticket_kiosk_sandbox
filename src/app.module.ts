93774b705d8bb104cfe21b90dcf4fac768e2522b
Thirawoot.P <thirawoot.put@gmail.com>
2025-01-03 15:37:56 +0700

    change folder name

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TicketApiModule } from './http/ticket-api.module';
+import { TicketApiModule } from './ticket-api/ticket-api.module';
 
 @Module({
   imports: [TicketApiModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
