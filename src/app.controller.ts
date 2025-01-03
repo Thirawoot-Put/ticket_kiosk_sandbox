f6228da279efaa72332fdebde3fb9651c3a966d8
Thirawoot.P <thirawoot.put@gmail.com>
2025-01-03 13:44:45 +0700

    health-check

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/health-check')
+  @HttpCode(200)
+  healthCheck(@Res() res: Response) {
+    const msg = this.appService.healthCheck();
+    res.json({ message: msg });
   }
 }
